7881cc6c7ad23ade3397f6dcc05bae9cf8d0cdb6
Andrew Sellergren <asellerg@gmail.com>
2023-11-04 16:52:31 -0700

    Updated styles and new EPICAC model.

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -3,6 +3,7 @@
 import {
   Configuration,
   OpenAIApi,
+  ChatCompletionRequestMessage,
   ChatCompletionRequestMessageRoleEnum,} from "openai";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -17,22 +18,30 @@ async function chatHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const instructions = `
-    Your name is Gambol.
+  let instructions = [
+    `Your name is Gambol.
     You are an expert at Texas Hold'em poker specifically the version called 6max.
-    You know perfect game-theory optimal strategy.
-    This is a poker hand history: ${req.body.userInput} and these are the GTO strategy percentages: ${req.body.strategy}.
-    When I type "Gambol: " you should respond with the GTO strategy percentages at the top and explain using poker theory (for example, talking about the opponents' likely ranges) why this is the GTO strategy. Gambol:
-    `
+    You know perfect game-theory optimal strategy.`
+  ]
+  const handHistory = req.body.firstQuestion ? req.body.userInput : req.body.handHistory;
+  instructions.push(
+      `This is a poker hand history: ${handHistory} and these are the GTO strategy percentages: ${req.body.strategy}.
+      You should first respond with the GTO strategy percentages at the top and explain using poker theory (for example, talking about the opponents' likely ranges) why this is the GTO strategy.
+      Then answer any follow-up questions the user might have.`
+  )
+  let messages: Array<ChatCompletionRequestMessage> = instructions.map(i => ({
+    role: ChatCompletionRequestMessageRoleEnum.System,
+    content: i,
+  }));
+  if (!req.body.firstQuestion) {
+    messages.push({
+      role: ChatCompletionRequestMessageRoleEnum.User,
+      content: req.body.userInput,
+    });
+  }
   const completion = await openai.createChatCompletion({
     model: "gpt-4",
-    messages: [
-      {
-        role: ChatCompletionRequestMessageRoleEnum.System,
-        content: instructions,
-      },
-      
-    ],
+    messages: messages,
     temperature: 0,
   });
   res.status(200).json({ result: completion.data.choices[0].message });
